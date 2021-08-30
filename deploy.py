31fe39136595e6a621174bec98a69b1dfe4ed075
jie <89714756+hello2jie@users.noreply.github.com>
2021-08-30 18:54:03 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,36 +1,51 @@
 from flask import Flask
 from flask import request
-import json 
-import git 
+import json
+import git
 import os
 import subprocess
+import shutil
 
 
-PROJECT_DIR = ''
+# Repo
+REPO_URL = "https://github.com/hello2jie/github_ci.git"
+# 项目路径
+PROJECT_DIR = '/home/van/github_ci'
+# 编译生成路径
 DIST_DIR = os.path.join(PROJECT_DIR, 'dist')
-RELEASE_DIR = ''
+# 部署地址
+RELEASE_DIR = '/home/van/front'
+
 
 def prepare():
-    print("start sync")
-    g = git.cmd.Git(PROJECT_DIR)    
-    g.pull()
+    print("start clone")
+    g = git.Repo.clone_from(url=REPO_URL, to_path=PROJECT_DIR)
     print("pull over.")
 
-def deploy():
-    prepare()
-    # cmd = ['cd', PROJECT_DIR, '&&','npm', 'run', 'build']
-    cmd = ['cd', PROJECT_DIR]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    for line in p.stdout:
-        print(line)
-    p.wait()
-    print(p.returncode)
 
-    
+def build():
+    print("start build...")
+    os.chdir(PROJECT_DIR)
+    subprocess.call(
+        f"npm install && npm run build && cp {DIST_DIR}/* -rf {RELEASE_DIR}", shell=True)
+
+
+def clean():
+    if os.path.isdir(PROJECT_DIR):
+        shutil.rmtree(PROJECT_DIR)
+
+
+def deploy():
+    clean()
+    prepare()
+    build()
+
+
 # webhook
 app = Flask(__name__)
 
-@app.route('/ci', methods = ['POST'])
+
+@app.route('/ci', methods=['POST'])
 def ci():
     payload = request.form.get('payload')
     if payload:
@@ -40,7 +55,7 @@ def ci():
         if ref.startswith('refs/tags') and base_ref == 'refs/heads/main':
             print('start deploy')
             deploy()
-        else: 
+        else:
             print('ignore commit')
     return 'ok'
 
